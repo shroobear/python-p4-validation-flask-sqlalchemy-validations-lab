0a7146a983a5c38a175504040a2a2e41b434936c
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-09-12 20:39:30 -0400

    lab complete

--- server/models.py
+++ server/models.py
@@ -12,6 +12,18 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Author must have a name")
+        return name
+
+    @validates('phone_number')
+    def validate_phone(self, key, number):
+        if len(number) != 10:
+            raise ValueError("Phone number must be 10 digits long")
+        return number
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -26,6 +38,33 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('content')
+    def validate_post_length(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Post must be at least 250 characters")
+        return content
+    
+    @validates('summary')
+    def validate_summary_length(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError("Summary must be less than 250 characters.")
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError(f'Category {category} not found.')
+        return category
+    
+    @validates('title')
+    def validate_clickbait_title(self, key, title):
+        clickbait_list = ["Won't Believe", "Secret", "Top", "Guess"]
+        for phrase in clickbait_list:
+            if phrase in title:
+                return phrase
+        raise ValueError('Title not clickbaity enough.')
+        
 
 
     def __repr__(self):
